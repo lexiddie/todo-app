e6802deae392934f4ce20905b9dd8b7d84bdbabf
Lex <vatharalin@gmail.com>
2020-04-01 16:19:09 +0700

    init

---
+++ src/ToDo.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+require('dotenv-flow').config();
+
+function ToDoList(props) {
+  //   const [tasks, setTasks] = useState([]);
+  const [list, setList] = useState([]);
+  const uri = process.env.REACT_APP_API_URI;
+  console.log(uri);
+  useEffect(() => {
+    fetch(`${uri}task`)
+      .then(res => res.json())
+      .then(data => {
+        // setTasks(res.data);
+        console.log(data);
+        let tasks = data;
+        let L = tasks['data'].map((v, i) => {
+          return <li key={i}>{v.name}</li>;
+        });
+        setList(L);
+      });
+  }, []);
+  return (
+    <div>
+      <h2>Things To Do</h2>
+      <ul>{list}</ul>
+    </div>
+  );
+}
+export default ToDoList;
