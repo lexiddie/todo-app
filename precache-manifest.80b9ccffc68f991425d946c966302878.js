ec7fc305914a22187c00a741b17bb8ac6485ba88
Lex <vatharalin@gmail.com>
2020-04-01 22:37:47 +0700

    Updates

---
+++ precache-manifest.80b9ccffc68f991425d946c966302878.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d1927b0e300e37c5fc2ee2d00876cd",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "ccd8f4a56e04140dfe6a",
+    "url": "/todo-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5cb18f397824c8087924",
+    "url": "/todo-app/static/js/2.a0174850.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.a0174850.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccd8f4a56e04140dfe6a",
+    "url": "/todo-app/static/js/main.5729441f.chunk.js"
+  },
+  {
+    "revision": "692b1522dada438b35b5",
+    "url": "/todo-app/static/js/runtime-main.c0b87c71.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
